f020ff0cee2570ec2447b73bbc696414cab5a7a5
N0len-sasha <79309214+N0len-sasha@users.noreply.github.com>
2024-02-20 09:44:28 +0300

    Update views.py

--- src/api/views.py
+++ src/api/views.py
@@ -1,5 +1,6 @@
 from djoser.views import UserViewSet
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
 
 from events.models import Event
@@ -7,8 +8,7 @@ from users.models import Friend, User
 
 from .pagination import MyPagination
 from .serializers import EventSerializer, FriendSerializer, MyUserSerializer
-from .filters import (UserFilter, EventsFilter,
-                      UserSearchFilter, EventSearchFilter)
+from .filters import (UserFilter, EventsFilter, EventSearchFilter)
 
 
 class MyUserViewSet(UserViewSet):
@@ -17,8 +17,9 @@ class MyUserViewSet(UserViewSet):
     queryset = User.objects.all()
     serializer_class = MyUserSerializer
     pagination_class = MyPagination
-    filter_backends = (UserSearchFilter, DjangoFilterBackend)
+    filter_backends = (filters.SearchFilter, DjangoFilterBackend)
     filterset_class = UserFilter
+    search_fields = ('first_name', 'last_name')
 
 
 class FriendViewSet(ModelViewSet):
