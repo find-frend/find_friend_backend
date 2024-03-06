cd7465b76fcffdd7b01ab927a4f7f53691fb31d8
Ilya Kutilin <ilyakutilin@yandex.ru>
2024-03-06 14:34:43 +0300

    Slight change to urls

--- src/api/urls.py
+++ src/api/urls.py
@@ -15,22 +15,16 @@ app_name = "api"
 
 router = DefaultRouter()
 
-# Добавление эндпоинтов для восстановления пароля в авто-swagger
-add_reset_password_urls_to_router(
-    router, base_path="auth/users/reset_password/"
-)
 
 router.register("users", MyUserViewSet, basename="users")
 router.register("events", EventViewSet, basename="events")
 router.register("interests", InterestViewSet, basename="interests")
 router.register(r"friends", FriendRequestViewSet, basename="friends")
 
+add_reset_password_urls_to_router(router, base_path="users/reset_password")
+
 urlpatterns = [
     path("", include(router.urls)),
-    path(
-        "auth/users/reset_password/",
-        include("django_rest_passwordreset.urls", namespace="password_reset"),
-    ),
     path("auth/", include("djoser.urls")),
     path("auth/", include("djoser.urls.authtoken")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
