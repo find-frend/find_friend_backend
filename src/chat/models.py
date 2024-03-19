2168f78bcba47765b7e700bd9a5a7f61dd8cd964
Ilya Kutilin <ilyakutilin@yandex.ru>
2024-03-19 16:59:53 +0300

    Add verbose_names to models

--- src/chat/models.py
+++ src/chat/models.py
@@ -11,14 +11,23 @@ class Chat(models.Model):
         on_delete=models.SET_NULL,
         null=True,
         related_name="started_chats",
+        verbose_name="Инициатор",
     )
     receiver = models.ForeignKey(
         User,
         on_delete=models.SET_NULL,
         null=True,
         related_name="participates_in_chats",
+        verbose_name="Получатель",
     )
-    start_time = models.DateTimeField(auto_now_add=True)
+    start_time = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Время создания чата",
+    )
+
+    class Meta:
+        verbose_name = "Чат"
+        verbose_name_plural = "Чаты"
 
     def __str__(self):
         return f"{self.initiator} - {self.receiver}"
@@ -32,17 +41,28 @@ class Message(models.Model):
         on_delete=models.SET_NULL,
         null=True,
         related_name="message_sender",
+        verbose_name="Отправитель",
     )
-    text = models.CharField(max_length=200, blank=True)
+    text = models.CharField(
+        "Текст",
+        max_length=200,
+        blank=True,
+    )
     chat = models.ForeignKey(
         Chat,
         on_delete=models.CASCADE,
         related_name="chat_messages",
+        verbose_name="Чат",
     )
-    timestamp = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Время отправки",
+    )
+
+    class Meta:
+        verbose_name = "Сообщение"
+        verbose_name_plural = "Сообщения"
+        ordering = ("-timestamp",)
 
     def __str__(self):
         return f"{self.sender} - {self.timestamp}"
-
-    class Meta:
-        ordering = ("-timestamp",)
