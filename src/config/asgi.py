334281fe9d43a55043ae3bc8c44974069ebdaacb
Ilya Kutilin <ilyakutilin@yandex.ru>
2024-03-20 14:55:39 +0300

    Disable AllowedHostsOriginValidator in ASGI settings

--- src/config/asgi.py
+++ src/config/asgi.py
@@ -1,7 +1,6 @@
 import os
 
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 from chat import routing
@@ -15,8 +14,8 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
-        "websocket": AllowedHostsOriginValidator(
-            TokenAuthMiddleware(URLRouter(routing.websocket_urlpatterns))
+        "websocket": TokenAuthMiddleware(
+            URLRouter(routing.websocket_urlpatterns)
         ),
     }
 )
