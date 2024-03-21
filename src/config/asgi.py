b3acd787eb9d5c52811bf0e9f8a2a0380d0190fc
Ilya Kutilin <ilyakutilin@yandex.ru>
2024-03-21 10:34:53 +0300

    Rearrange imports and code in asgi.py to properly launch daphne

--- src/config/asgi.py
+++ src/config/asgi.py
@@ -1,15 +1,26 @@
-import os
+"""
+Настройки ASGI сервера.
 
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
+<=!!! ВНИМАНИЕ! ПОРЯДОК ИМПОРТОВ И СТРОК КОДА ВАЖЕН И НЕ ДОЛЖЕН МЕНЯТЬСЯ !!!=>
 
-from chat import routing
-from chat.middleware import TokenAuthMiddleware
+Необходимо инициализировать приложение Djando ASGI как можно раньше,
+чтобы AppRegistry был заполнен до того, как последующие импорты
+повлекут за собой импорт ORM моделей.
+"""
+
+from django.core.asgi import get_asgi_application  # noqa
+
+django_asgi_app = get_asgi_application()  # noqa
+
+import os  # noqa
+
+from channels.routing import ProtocolTypeRouter, URLRouter  # noqa
+
+from chat import routing  # noqa
+from chat.middleware import TokenAuthMiddleware  # noqa
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
-django_asgi_app = get_asgi_application()
+
 
 application = ProtocolTypeRouter(
     {
