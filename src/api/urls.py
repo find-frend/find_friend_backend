60e900cef7ab018c594a2fd95d3b270e050594ba
Korsunov Viktor <viktorkorsunov2001@gmail.com>
2024-03-26 19:30:42 +0300

    Add notifications url.

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,17 +1,12 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import include, path
+
 from django_rest_passwordreset.urls import add_reset_password_urls_to_router
 
 from .routers import CustomRouter
-from .views import (
-    CityViewSet,
-    EventViewSet,
-    FriendRequestViewSet,
-    InterestViewSet,
-    MyUserViewSet,
-)
-
+from .views import (CityViewSet, EventViewSet, FriendRequestViewSet,
+                    InterestViewSet, MyUserViewSet, NotificationViewSet)
 
 app_name = "api"
 
@@ -24,9 +19,11 @@ router.register("events", EventViewSet, basename="events")
 router.register("interests", InterestViewSet, basename="interests")
 router.register("cities", CityViewSet, basename="cities")
 router.register(r"friends", FriendRequestViewSet, basename="friends")
+router.register(r"notification", NotificationViewSet, basename="notification")
 
 
 urlpatterns = [
     path("", include(router.urls)),
     path("auth/", include("djoser.urls.authtoken")),
+    path("chats/", include("chat.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
